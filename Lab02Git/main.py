3ce8973f205ffbbdb3170af5cb232bdbd9113ace
Paibo. <kesskeetit@mail.ru>
2024-09-13 09:02:04 +0500

    Add Eratosphen algorithm

---
+++ Lab02Git/main.py
@@ -0,0 +1,15 @@
+def Erat(n):
+    primes = [i for i in range(n + 1)]
+    primes[1] = 0
+    i = 2
+    while i <= n:
+        if primes[i] != 0:
+            j = i + i
+            while j <= n:
+                primes[j] = 0
+                j = j + i
+        i += 1
+    primes = [i for i in primes if i != 0]
+    print(primes)
+
+Erat(5)
